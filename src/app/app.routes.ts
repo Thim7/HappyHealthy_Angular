5fb37aee921a9cfea6a19a6d14fcacd242d9ad90
Thim7 <93660448+Thim7@users.noreply.github.com>
2024-10-25 21:00:22 +0700

    feat: build route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent },
+  { path: '', component: LandingComponent },
+  { path: '**', component: NotFoundComponent },
+];
