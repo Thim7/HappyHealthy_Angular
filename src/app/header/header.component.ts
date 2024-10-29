c9c597dd9754aecac126bf2ea831bd7abe461647
Thim7 <93660448+Thim7@users.noreply.github.com>
2024-10-29 20:29:51 +0700

    feat: build base for landing page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, NgOptimizedImage],
+  imports: [MatButtonModule, NgOptimizedImage, MatTabsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
