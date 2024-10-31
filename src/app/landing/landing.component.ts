e5c634f65ac132128003b9c9fd1bcb44a2402183
Thim7 <93660448+Thim7@users.noreply.github.com>
2024-10-31 22:06:20 +0700

    feat (ui): build hero image

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.css',
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
